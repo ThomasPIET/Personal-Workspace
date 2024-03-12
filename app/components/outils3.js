078b007fb17fa459151e6bd7fc8329c77dce9d14
PIET Thomas <thomas.piet@ynov.com>
2024-03-12 10:22:19 +0100

    Add YouTubePlayer component

--- app/components/outils3.js
+++ app/components/outils3.js
@@ -1,11 +1,57 @@
 import ToolCard from "../UI/card";
-
+import YouTubePlayer from "youtube-player";
+import React, { useEffect, useRef, useState } from "react";
 
 export default function Outils3() {
+  const playerRef = useRef(null);
+  const [player, setPlayer] = useState(null);
+
+  useEffect(() => {
+    const player1 = YouTubePlayer(playerRef.current, {
+      videoId: "jfKfPfyJRdk",
+    });
+
+    setPlayer(player1);
+
+    if (player1) {
+      player1.playVideo().then(function () {
+        console.log(
+          "Starting to play player1. It will take some time to buffer video before it starts playing."
+        );
+      });
+    }
+  }, []);
+
+  // const handlePlay = () => {
+  //   console.log("Play button clicked");
+  //   if (player) {
+  //     player.unMute().then(function () {
+  //       console.log("Sound is on");
+  //     });
+  //   } else {
+  //     console.log("Player is not initialized yet");
+  //   }
+  // };
+  
+  // const handlePause = () => {
+  //   console.log("Pause button clicked");
+  //   if (player) {
+  //     player.mute().then(function () {
+  //       console.log("Sound is off");
+  //     });
+  //   } else {
+  //     console.log("Player is not initialized yet");
+  //   }
+  // };
+
   return (
     <ToolCard>
-      WORK IN PROGRESS
-    
+      <div style={{ display: "none" }} ref={playerRef}></div>
+      {/* <div>
+        <p>Listening lofi</p>
+        <button onClick={handlePlay}>Play</button>
+        <button onClick={handlePause}>Pause</button>
+      </div> */}
     </ToolCard>
   );
 }
